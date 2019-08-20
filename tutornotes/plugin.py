15518f0c598c3f23151381f43e10fe0e1159e465
Régis Behmo <regis@behmo.com>
2019-08-20 18:41:52 +0200

    :sunrise:

---
+++ tutornotes/plugin.py
@@ -0,0 +1,41 @@
+from glob import glob
+import os
+
+import pkg_resources
+
+from .__about__ import __version__
+
+
+config = {
+    "add": {
+        "MYSQL_PASSWORD": "{{ 8|random_string }}",
+        "SECRET_KEY": "{{ 24|random_string }}",
+        "OAUTH2_SECRET": "{{ 24|random_string }}",
+    },
+    "defaults": {
+        "VERSION": __version__,
+        "DOCKER_IMAGE": "overhangio/openedx-notes:{{ NOTES_VERSION }}",
+        "HOST": "notes.{{ LMS_HOST }}",
+        "MYSQL_DATABASE": "notes",
+        "MYSQL_USERNAME": "notes",
+    },
+}
+
+templates = pkg_resources.resource_filename("tutornotes", "templates")
+hooks = {
+    "init": ["mysql-client", "lms", "notes"],
+    "build-image": {"notes": "{{ NOTES_DOCKER_IMAGE }}"},
+    "remote-image": {"notes": "{{ NOTES_DOCKER_IMAGE }}"},
+}
+
+
+def patches():
+    all_patches = {}
+    for path in glob(
+        os.path.join(pkg_resources.resource_filename("tutornotes", "patches"), "*")
+    ):
+        with open(path) as patch_file:
+            name = os.path.basename(path)
+            content = patch_file.read()
+            all_patches[name] = content
+    return all_patches
