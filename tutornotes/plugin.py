47c90a84097e371d718b58d87da2a8614ca25344
Régis Behmo <regis@behmo.com>
2022-05-10 12:18:20 +0200

    feat: upgrade to nutmeg

--- tutornotes/plugin.py
+++ tutornotes/plugin.py
@@ -3,11 +3,13 @@ import os
 
 import pkg_resources
 
+from tutor import hooks as tutor_hooks
+
 from .__about__ import __version__
 
 
 config = {
-    "add": {
+    "unique": {
         "MYSQL_PASSWORD": "{{ 8|random_string }}",
         "SECRET_KEY": "{{ 24|random_string }}",
         "OAUTH2_SECRET": "{{ 24|random_string }}",
@@ -21,21 +23,68 @@ config = {
     },
 }
 
-templates = pkg_resources.resource_filename("tutornotes", "templates")
-hooks = {
-    "init": ["mysql", "lms", "notes"],
-    "build-image": {"notes": "{{ NOTES_DOCKER_IMAGE }}"},
-    "remote-image": {"notes": "{{ NOTES_DOCKER_IMAGE }}"},
-}
+# Initialization hooks
+tutor_hooks.Filters.COMMANDS_INIT.add_item((
+    "mysql",
+    ("notes", "tasks", "mysql", "init"),
+))
+tutor_hooks.Filters.COMMANDS_INIT.add_item((
+    "lms",
+    ("notes", "tasks", "lms", "init"),
+))
+tutor_hooks.Filters.COMMANDS_INIT.add_item((
+    "notes",
+    ("notes", "tasks", "notes", "init"),
+))
 
+# Image management
+tutor_hooks.Filters.IMAGES_BUILD.add_item((
+    "notes",
+    ("plugins", "notes", "build", "notes"),
+    "{{ NOTES_DOCKER_IMAGE }}",
+    (),
+))
+tutor_hooks.Filters.IMAGES_PULL.add_item((
+    "notes",
+    "{{ NOTES_DOCKER_IMAGE }}",
+))
+tutor_hooks.Filters.IMAGES_PUSH.add_item((
+    "notes",
+    "{{ NOTES_DOCKER_IMAGE }}",
+))
 
-def patches():
-    all_patches = {}
-    for path in glob(
-        os.path.join(pkg_resources.resource_filename("tutornotes", "patches"), "*")
-    ):
-        with open(path) as patch_file:
-            name = os.path.basename(path)
-            content = patch_file.read()
-            all_patches[name] = content
-    return all_patches
+####### Boilerplate code
+# Add the "templates" folder as a template root
+tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    pkg_resources.resource_filename("tutornotes", "templates")
+)
+# Render the "build" and "apps" folders
+tutor_hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("notes/build", "plugins"),
+        ("notes/apps", "plugins"),
+    ],
+)
+# Load patches from files
+for path in glob(
+    os.path.join(
+        pkg_resources.resource_filename("tutornotes", "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+# Add configuration entries
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        (f"NOTES_{key}", value)
+        for key, value in config.get("defaults", {}).items()
+    ]
+)
+tutor_hooks.Filters.CONFIG_UNIQUE.add_items(
+    [
+        (f"NOTES_{key}", value)
+        for key, value in config.get("unique", {}).items()
+    ]
+)
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items(list(config.get("overrides", {}).items()))
